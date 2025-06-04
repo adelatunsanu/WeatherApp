eb0c83876e20711f4d415727a0c0a2a2aac91cf2
Adela Tunsanu <adelatunsanu@gmail.com>
2025-06-04 16:50:31 +0300

    Create basic kafka producer

---
+++ kafka-weather/src/main/java/org/example/WeatherProducer.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+/**
+ * The Kafka Producer.
+ */
+public class WeatherProducer {
+
+    private static final String TOPIC = "weather-forecast";
+    private static final String KAFKA_SERVER = "localhost:9092";
+
+    public static void main (String[] args) {
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
+    }
+}
