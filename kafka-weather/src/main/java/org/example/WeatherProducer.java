6f55210644be2cb10adff6b94ce78abc17a87a69
Adela Tunsanu <adelatunsanu@gmail.com>
2025-06-04 18:12:34 +0300

    Schedule task to fetch and send data to kafka

--- kafka-weather/src/main/java/org/example/WeatherProducer.java
+++ kafka-weather/src/main/java/org/example/WeatherProducer.java
@@ -2,15 +2,25 @@ package org.example;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  * The Kafka Producer.
  */
 public class WeatherProducer {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(WeatherProducer.class.getSimpleName());
     private static final String TOPIC = "weather-forecast";
     private static final String KAFKA_SERVER = "localhost:9092";
 
@@ -22,5 +32,28 @@ public class WeatherProducer {
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         KafkaProducer<String, String> kafkaProducer = new KafkaProducer<>(properties);
+
+        try (ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor()) {
+            Runnable task = new Runnable() {
+                @Override
+                public void run () {
+                    LOGGER.info("Weather producer running. Fetching every 1 minute...");
+                    try {
+                        ProducerRecord<String, String> record = null;
+                        List<String> forecastList = OpenMeteoClient.getHourlyWeatherData(40.46, 24.36);
+                        for (String jsonData : forecastList) {
+                            record = new ProducerRecord<>(TOPIC, jsonData);
+                            kafkaProducer.send(record);
+                            LOGGER.info("Sent to Kafka: {}", jsonData);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            };
+
+
+            executorService.scheduleAtFixedRate(task, 0, 1, TimeUnit.MINUTES);
+        }
     }
 }
