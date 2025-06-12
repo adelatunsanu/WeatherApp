afb816d355e7f5567f34e3e3d5aa58c4adbb6773
Adela Tunsanu <adelatunsanu@gmail.com>
2025-06-12 16:21:08 +0300

    Calculate average temperature forecast per city

--- kafka-weather/src/main/java/org/example/WeatherConsumer.java
+++ kafka-weather/src/main/java/org/example/WeatherConsumer.java
@@ -1,16 +1,15 @@
 package org.example;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.kafka.clients.consumer.*;
 import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.time.Duration;
-import java.util.Collection;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 /**
  * The {@code WeatherConsumer} class is a Kafka consumer that subscribes to a topic and logs received weather data.
@@ -21,6 +20,7 @@ public class WeatherConsumer {
     private static final String TOPIC = "weather-forecast";
     private static final String KAFKA_SERVER = "localhost:9092";
     private static final String GROUP_ID = "weather-consumer-group";
+    private static final ObjectMapper MAPPER = new ObjectMapper();
 
     public static void main (String[] args) {
         Properties properties = getProperties();
@@ -55,12 +55,29 @@ public class WeatherConsumer {
             });
 
             try {
+                // Group temperatures by city
+                Map<String, List<Double>> temperaturePerCity = new HashMap<>();
+
                 while (true) {
                     ConsumerRecords<String, String> records = kafkaConsumer.poll(Duration.ofSeconds(1));
+
                     for (ConsumerRecord<String, String> record : records) {
                         LOGGER.info("Received weather data: {} ----> Partition: {} Offset: {} Timestamp: {}", record.value(), record.partition(), record.offset(), record.timestamp());
-                        // TODO - process data
+
+                        String city = record.key();
+                        String json = record.value();
+                        try{
+                            JsonNode node = MAPPER.readTree(json);
+                            double temperature = node.get("temperature").asDouble();
+
+                            temperaturePerCity.computeIfAbsent(city, k -> new ArrayList<>()).add(temperature);
+                        } catch (Exception e) {
+                            LOGGER.error("Failed to parse JSON for record: {}", json, e);
+                        }
                     }
+
+                    calculateAverageTemperaturesPerCity(temperaturePerCity);
+
                 }
             } catch (WakeupException exception) {
                 LOGGER.info("Kafka consumer wakeup triggered. Shutting down gracefully...");
@@ -80,5 +97,20 @@ public class WeatherConsumer {
         properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
         properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
         return properties;
+    }
+
+    private static void calculateAverageTemperaturesPerCity(Map<String, List<Double>> tempPerCity){
+        for (Map.Entry<String, List<Double>> entry : tempPerCity.entrySet()) {
+            String city = entry.getKey();
+            List<Double> temps = entry.getValue();
+
+            double avg = temps.stream()
+                    .mapToDouble(Double::doubleValue)
+                    .average()
+                    .orElse(Double.NaN);
+
+            LOGGER.info("City: {} | Records received: {} | Average Temperature: {}°C",
+                    city, temps.size(), String.format("%.2f", avg));
+        }
     }
 }
